fee8f9c74bec377f292d15f0538c4de76f446b9a
DANXPS\ddres <ddresser05@gmail.com>
2024-12-10 17:06:38 -0600

    Solved day 9 part 2

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 pub fn load_input(file_path: &str) -> String {
     fs::read_to_string(file_path).expect("Could not read from file")
@@ -66,19 +66,72 @@ pub fn checksum_part_2(disk_map: &str) -> usize {
         }
     });
 
-    let mut i = 0;
     let mut j = block_representation.len() - 1;
+    let mut attempted_swapped_files = HashSet::new();
 
-    while i < j {
-        if block_representation[i] == "." {
-            while j > i && block_representation[j] == "." {
+    while j > 0 {
+        if block_representation[j] == "." {
+            j -= 1;
+            continue;
+        } else if let Ok(number) = block_representation[j].parse::<usize>() {
+            if attempted_swapped_files.contains(&number) {
+                j -= 1;
+                continue;
+            } else {
+                attempted_swapped_files.insert(number);
+            }
+
+            let number_count = block_representation
+                .iter()
+                .filter(|num| {
+                    if let Ok(n) = num.parse::<usize>() {
+                        n == number
+                    } else {
+                        false
+                    }
+                })
+                .count();
+
+            let mut free_space_start = None;
+            let mut free_space_count = 0;
+
+            for (i, value) in block_representation.iter().enumerate() {
+                if i >= j {
+                    break;
+                }
+                if value == "." {
+                    if free_space_start.is_none() {
+                        free_space_start = Some(i);
+                    }
+                    free_space_count += 1;
+                    if free_space_count >= number_count {
+                        break;
+                    }
+                } else {
+                    free_space_start = None;
+                    free_space_count = 0;
+                }
+            }
+
+            if let Some(start) = free_space_start {
+                for k in 0..number_count {
+                    if number_count <= free_space_count {
+                        block_representation.swap(start + k, j);
+                        j -= 1;
+                        if j == 0 {
+                            break;
+                        }
+                    } else {
+                        j -= 1;
+                        if j == 0 {
+                            break;
+                        }
+                    }
+                }
+            } else {
                 j -= 1;
             }
-            if i < j {
-                block_representation.swap(i, j);
-            }
         }
-        i += 1;
     }
 
     block_representation
@@ -113,6 +166,12 @@ mod tests {
     #[test]
     fn day_09_part_2_simple() {
         let input = load_input("input_simple.txt");
-        assert_eq!(checksum(&input), 2858);
+        assert_eq!(checksum_part_2(&input), 2858);
+    }
+
+    #[test]
+    fn day_09_part_2() {
+        let input = load_input("input.txt");
+        assert_eq!(checksum_part_2(&input), 6636608781232);
     }
 }
